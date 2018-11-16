faef0e8814af8f89d63d22d805bc70452384da58
blkopsdev <blkopsdev@gmail.com>
2018-11-16 13:57:55 -0500

    disabled ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        URL::forceScheme('https');
+        URL::forceScheme('http');
     }
 
     /**
